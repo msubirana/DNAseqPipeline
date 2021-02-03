58ef788dd3e2bcc68c0844d846f0053ee7bbb297
mb2subi@gmail.com <mb2subi@gmail.com>
2021-02-03 16:51:53 +0100

    added marvinParser.R

---
+++ R/marvinParser.R
@@ -0,0 +1,65 @@
+#' marvinParser
+#'
+#' Parser for Marvin cluster
+#' It is necessary generate a public and private key pair for an automatic scp
+#' @param cores Cores to use
+#' @param job_name Name of the job
+#' @param partition partition where to run the script. By default 'partition'. 
+#' @param script Script to run
+#' @param memory Amount of RAM used per cpu. By default '8G'.
+#' @param email Email where to send notifications. By default 'clusterigtpmsubirana@gmail.com'
+#' @param modules List of modules to load. Check the once that you need using module av command in Marvin. 
+#' @param tmp_sh File where to save the sh script in the host. 
+#' @examples
+#' \dontrun{}
+#' @export
+marvinParser <- function(job_name,
+                         cores=1,
+                         partition='normal',
+                         memory='8G',
+                         email='clusterigtpmsubirana@gmail.com',
+                         modules=NULL,
+                         script,
+                         tmp_sh){
+  
+  #TODO which partitions exist?
+  #TODO log path
+  # define tmp file for running in SGE
+  if (!is.null(modules)){
+    load_modules <- paste('module load', modules, collapse = '\n')}
+  
+  cmd <- paste0('#!/bin/bash\n', 
+                '# Job name identification\n',
+                '#SBATCH -J ', job_name, '\n',
+                '# set the partition where the job will run\n',
+                '#SBATCH --partition=', partition,'\n',
+                '# Number of cores\n',
+                '#SBATCH -n', cores, '\n',
+                '# Send an email if the job\n',
+                '#SBATCH --mail-user=', email, '\n',
+                '#+ starts / ends / fails\n',
+                '#SBATCH --mail-type=BEGIN,END,FAIL\n', 
+                '# memory per CPU\n',
+                '#SBATCH --mem-per-cpu=', memory, '\n',
+                '#load modules\n',
+                load_modules,
+                '\n#run application\n',
+                script)
+  
+  write(cmd,
+        file=tmp_sh)
+  
+  system(paste('scp',
+               tmp_sh,
+               'msubirana@marvin.s.upf.edu:/gpfs42/projects/lab_lpasquali/shared_data/marc/tmp/marvinParserScripts'))
+  
+  tmp_sh_marvin <- file.path('/gpfs42/projects/lab_lpasquali/shared_data/marc/tmp/marvinParserScripts', basename(tmp_sh))
+  
+  ssh_connection <- ssh::ssh_connect('msubirana@marvin.s.upf.edu')
+  ssh::ssh_exec_internal(ssh_connection, command = paste('sbatch', tmp_sh_marvin))
+  
+}
+
+
+
+
--- tmp/realignBwaEpimutations20210203.R
+++ tmp/realignBwaEpimutations20210203.R
@@ -1,20 +1,36 @@
-devtools::load_all('/homes/users/msubirana/lab_lpasquali/shared_data/marc/repos/DNAseqPipeline')
+devtools::load_all('/home/msubirana/Documents/phd/repos/DNAseqPipeline')
 ref <- '/gpfs42/projects/lab_lpasquali/shared_data/marc/ref/hg38/hg38.fa'
 bam_path <- '/homes/users/msubirana/lab_lpasquali/shared_data/marc/epimutations/raw/wgs/hg37/toDo'
-
 bams <- list.files(bam_path,
                    pattern = ".bam$",
                    full.names = T)
-
 out_dir <- '/homes/users/msubirana/lab_lpasquali/shared_data/marc/epimutations/raw/wgs/hg38'
-threads <- 8
+threads <- 16
+cores=threads/2
+modules <- c('BWA/0.7.15',
+             'msamblaster/0.1.24-foss-2016b',
+             'SAMtools/1.11-GCC-9.3.0',
+             'R/4.0.0-foss-2020a')
+
+tmp_sh <- '/home/msubirana/tmp.sh'
 
 for(bam in bams){
 
-    realignBwa(bam=bam,
-             ref=ref,
-             out_dir=out_dir,
-             threads=threads)
-  
+  sample <- gsub('.bam','',bam)
+  job_name = paste0(sample_name, '_realignBwa')
+  script = paste('Rscript /gpfs42/projects/lab_lpasquali/shared_data/marc/repos/DNAseqPipeline/R/realignBwaParser.R',
+                 bam,
+                 ref,
+                 out_dir,threads)
+  email = 'clusterigtpmsubirana@gmail.com'
+
+  marvinParser(job_name=job_name,
+               cores=cores,
+               modules=modules,
+               script=script,
+               tmp_sh=tmp_sh)
+
+
+
 }
 
