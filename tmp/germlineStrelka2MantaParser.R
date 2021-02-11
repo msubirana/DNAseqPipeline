9b5865df6a0e8494c4b40b4a13b57659518cbf7c
mb2subi@gmail.com <mb2subi@gmail.com>
2021-02-11 12:24:43 +0100

    Changed out_dir for out_dir_manta in germlineStrelka2Manta.R

--- tmp/germlineStrelka2MantaParser.R
+++ tmp/germlineStrelka2MantaParser.R
@@ -15,7 +15,6 @@ cores <- parallel::detectCores()/2
 #realign BAMs using DNAseqPipeline::realignBwa
 germlineStrelka2Manta(bam,
                       ref=ref,
-                      out_dir=out_dir,
                       cores=cores,
                       out_dir_manta=out_dir_manta,
                       out_dir_strelka2=out_dir_strelka2)
