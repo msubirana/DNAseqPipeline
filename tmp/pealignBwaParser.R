2229d1df4edac9daef326609562cee422950e1e8
mb2subi@gmail.com <mb2subi@gmail.com>
2021-02-18 14:53:47 +0100

    added pealignBwaParser.R

---
+++ tmp/pealignBwaParser.R
@@ -0,0 +1,20 @@
+#load package
+devtools::load_all('/gpfs42/projects/lab_lpasquali/shared_data/marc/repos/DNAseqPipeline')
+
+#parse arguments
+args <- base::commandArgs(trailingOnly = TRUE)
+
+#define variables
+fq1 <- args[1]
+fq2 <- args[2]
+ref <- args[3]
+out_dir <- args[4]
+threads <- parallel::detectCores()
+
+
+#realign BAMs using DNAseqPipeline::realignBwa
+pealignBwa(fq1=fq1,
+           fq2=fq2,
+           ref=ref,
+           out_dir=out_dir,
+           threads=threads)
