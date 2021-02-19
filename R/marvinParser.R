8f4f078a058fa69cd004d82f464444f368c31f3e
mb2subi@gmail.com <mb2subi@gmail.com>
2021-02-19 14:48:23 +0100

    cmd_shh marvinParser.R modified

--- R/marvinParser.R
+++ R/marvinParser.R
@@ -94,12 +94,9 @@ marvinParser <- function(job_name,
                'msubirana@marvin.s.upf.edu:/gpfs42/projects/lab_lpasquali/shared_data/marc/tmp/marvinParserScripts'))
 
   tmp_sh_marvin <- file.path('/gpfs42/projects/lab_lpasquali/shared_data/marc/tmp/marvinParserScripts', basename(tmp_sh))
-
   ssh_connection <- ssh::ssh_connect('msubirana@marvin.s.upf.edu')
+  stdout <- ssh::ssh_exec_internal(ssh_connection, command = paste('sbatch', tmp_sh_marvin))
 
-  cmd_ssh <- paste('sbatch', tmp_sh_marvin)
-
-  stdout <- ssh::ssh_exec_internal(ssh_connection, command = cmd_ssh)
   message(unlist(strsplit(rawToChar(stdout$stdout), '\n')))
 
 }
