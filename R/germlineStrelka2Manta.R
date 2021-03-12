5c759efe8295ef0168817e7df2df8a5dbe6e33fb
mb2subi@gmail.com <mb2subi@gmail.com>
2021-03-12 14:30:50 +0100

    corrected conf_strelka in germlineStrelka2Manta.R

--- R/germlineStrelka2Manta.R
+++ R/germlineStrelka2Manta.R
@@ -42,7 +42,7 @@ germlineStrelka2Manta <- function(bam,
            ref=ref,
            out_dir_strelka2=out_dir_strelka2,
            cores=cores,
-           conf_strelka2=conf_strelka,
+           conf_strelka2=conf_strelka2,
            call_regions=call_regions,
            indel_candidates=indel_candidates)
 
