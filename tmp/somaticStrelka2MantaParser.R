9e053ddb5ceae8284495e4c6c76f549fce3ceae4
mb2subi@gmail.com <mb2subi@gmail.com>
2021-05-05 11:27:17 +0200

    somaticStrelka2MantaParser.R tumor_bam error solved

--- tmp/somaticStrelka2MantaParser.R
+++ tmp/somaticStrelka2MantaParser.R
@@ -5,13 +5,13 @@ devtools::load_all('/gpfs42/projects/lab_lpasquali/shared_data/marc/repos/DNAseq
 args <- base::commandArgs(trailingOnly = TRUE)
 
 #define variables
-bam <- args[1]
-ref <- args[2]
-out_dir_manta <- args[3]
-out_dir_strelka2 <- args[4]
-call_regions <- args[5]
+tumor_bam <- args[1]
+ctrl_bam <- args[2]
+ref <- args[3]
+out_dir_manta <- args[4]
+out_dir_strelka2 <- args[5]
+call_regions <- args[6]
 cores <- parallel::detectCores()/2
-
 
 #calling varianyts using DNAseqPipeline::somaticStrelka2Manta
 somaticStrelka2Manta(tumor_bam=tumor_bam,
