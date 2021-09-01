cf8503af885563fc2b4d464236de7d3dbf0e5d04
mb2subi@gmail.com <mb2subi@gmail.com>
2021-09-01 10:44:06 +0200

    corrected out_mutect2_path in mutect2 script

--- R/mutect2.R
+++ R/mutect2.R
@@ -6,7 +6,7 @@
 #' @param tumor_bam Tumor BAM file to use in the analysis
 #' @param ref Reference fasta genome.
 #' @param gnomad GnomAD VCF
-#' @param out_pon_path PoN output path (it will create a vcf with the sm name as a name file)
+#' @param out_mutect2_path Output path (it will create a vcf with the sm name as a name file)
 #' @param gatk4 Gatk4 path
 #' @param samtools Samtools path
 #' @param cores Number of cores to use.
@@ -18,7 +18,7 @@ mutect2 <- function(ctrl_bam,
                                         tumor_bam,
                                         ref='/gpfs42/robbyfs/scratch/lab_lpasquali/msubirana/ref/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa',
                                         gnomad='/gpfs42/robbyfs/scratch/lab_lpasquali/msubirana/ref/af-only-gnomad.hg38.vcf.gz',
-                                        out_pon_path,
+                                        out_mutect2_path,
                                         cores,
                                         gatk4='gatk',
                                         samtools='samtools'){
@@ -28,7 +28,7 @@ mutect2 <- function(ctrl_bam,
     ctrl_bam_name <- unlist(strsplit(ctrl_bam_name, 'SM:'))[2]
 
     out_name <- gsub('_BL.bam', '', basename(ctrl_bam))
-    out_mutect2 <- file.path(out_pon_path, paste0(out_name, '.vcf.gz'))
+    out_mutect2 <- file.path(out_mutect2_path, paste0(out_name, '.vcf.gz'))
 
     message(paste(
       paste0('\n[', Sys.time(), ']'),
@@ -36,7 +36,7 @@ mutect2 <- function(ctrl_bam,
       '> Ctrl BAM file:\n', ctrl_bam, '\n',
       '> Tumor BAM file:\n', tumor_bam, '\n',
       '> Reference genome:', ref, '\n',
-      '> Output vcf:', out_pon, '\n'))
+      '> Output vcf:', out_mutect2_path, '\n'))
 
     system(paste(gatk4, '--java-options', paste0('"-XX:ParallelGCThreads=',cores*2,'"'),
                  'Mutect2',
--- tmp/mutect2Parser.R
+++ tmp/mutect2Parser.R
@@ -7,11 +7,11 @@ args <- base::commandArgs(trailingOnly = TRUE)
 #define variables
 ctrl_bam <- args[1]
 tumor_bam <- args[2]
-out_pon_path <- args[3]
+out_mutect2_path <- args[3]
 cores <- args[4]
 
 #calling varianyts using DNAseqPipeline::somaticStrelka2Manta
 mutect2(ctrl_bam=ctrl_bam,
                             tumor_bam=tumor_bam,
-                            out_pon_path=out_pon_path,
+                            out_mutect2_path=out_mutect2_path,
                             cores=cores)
