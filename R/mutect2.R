daf1ea7b3b1c504161a8ee65a78521b743b4784d
mb2subi@gmail.com <mb2subi@gmail.com>
2021-09-07 11:13:21 +0200

    upload mutect2 command with sample ctrl name

--- R/mutect2.R
+++ R/mutect2.R
@@ -9,6 +9,7 @@
 #' @param out_mutect2_path Output path (it will create a vcf with the sm name as a name file)
 #' @param gatk4 Gatk4 path
 #' @param samtools Samtools path
+#' @param ctrl_bam_name Sample name of the ctrl
 #' @param cores Number of cores to use.
 #' @examples
 #' \dontrun{}
@@ -16,16 +17,13 @@
 #TODO examples
 mutect2 <- function(ctrl_bam,
                                         tumor_bam,
+                                        ctrl_bam_name,
                                         ref='/gpfs42/robbyfs/scratch/lab_lpasquali/msubirana/ref/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa',
                                         gnomad='/gpfs42/robbyfs/scratch/lab_lpasquali/msubirana/ref/af-only-gnomad.hg38.vcf.gz',
                                         out_mutect2_path,
                                         cores,
                                         gatk4='gatk',
                                         samtools='samtools'){
-
-    ctrl_bam_name <- system(paste(samtools, 'view -H', ctrl_bam),intern = TRUE)
-    ctrl_bam_name <- ctrl_bam_name[grep('^@RG', ctrl_bam_name)]
-    ctrl_bam_name <- unlist(strsplit(ctrl_bam_name, 'SM:'))[2]
 
     out_name <- gsub('_BL.bam', '', basename(ctrl_bam))
     out_mutect2 <- file.path(out_mutect2_path, paste0(out_name, '.vcf'))
--- tmp/mutect2Parser.R
+++ tmp/mutect2Parser.R
@@ -7,11 +7,13 @@ args <- base::commandArgs(trailingOnly = TRUE)
 #define variables
 ctrl_bam <- args[1]
 tumor_bam <- args[2]
-out_mutect2_path <- args[3]
-cores <- args[4]
+ctrl_bam_name <- args[3]
+out_mutect2_path <- args[4]
+cores <- args[5]
 
 #calling varianyts using DNAseqPipeline::somaticStrelka2Manta
 mutect2(ctrl_bam=ctrl_bam,
-                            tumor_bam=tumor_bam,
-                            out_mutect2_path=out_mutect2_path,
-                            cores=cores)
+        tumor_bam=tumor_bam,
+        ctrl_bam_name=ctrl_bam_name,
+        out_mutect2_path=out_mutect2_path,
+        cores=cores)
